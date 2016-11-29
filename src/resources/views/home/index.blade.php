44bfc6c3446dc0bfe7a165db78439e652fdd10ba
LaurieZC <lawrencechangzc@gmail.com>
2016-11-29 22:25:38 +0800

    add

---
+++ src/resources/views/home/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts')
+
+@section('title')
+game
+@endsection
+
+@section('content')
+<table>
+    <tr>
+        <td>123</td>
+        <td>456</td>
+    </tr>
+</table>
+@endsection
---
+++ src/resources/views/layouts.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>xxx - @yield('title','页面')</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
