9e524662ff14968524a977c32be658161f72b393
lauriezc <lawrencechangzc@gmail.com>
2016-12-01 18:33:17 -0800

    add new js file for basic shape drawing

--- src/public/js/cvs.js
+++ src/public/js/cvs.js
@@ -1,6 +1,10 @@
 (function() {
 	var cvs = document.getElementById('cvs');
 	var ctx = cvs.getContext('2d');
-	ctx.fillRect(20,20,300,300);
-	ctx.stroke();
+	//ctx.fillRect(20,20,300,300);
+    ctx.fillStyle = 'rgb(191, 215, 255)';
+    ctx.beginPath();
+    ctx.arc(100, 75, 50, 0, 2*Math.PI);
+    ctx.fill();
+    ctx.stroke();
 })();
